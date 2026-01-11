5db06a713ed9657ad16bf3770bcb269a19bc92d0
stopazs <228461123+stopazus@users.noreply.github.com>
2026-01-11 06:21:02 -0500

    Add pop_any helper for identifiers

--- sar_parser/__init__.py
+++ sar_parser/__init__.py
@@ -1,8 +1,11 @@
 """Public API for the :mod:`sar_parser` package."""
 
+from .utils import load_identifier, pop_any
 from .validator import ValidationError, ValidationResult, validate_file, validate_string
 
 __all__ = [
+    "load_identifier",
+    "pop_any",
     "ValidationError",
     "ValidationResult",
     "validate_file",
